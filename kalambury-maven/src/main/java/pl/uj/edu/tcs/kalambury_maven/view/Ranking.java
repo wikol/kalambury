5c52c2609e7bd95175dc8d1358cb1b0861bcd7c6
mreve <aadymek@gmail.com>
2013-05-17 00:05:20 +0200

    Poprawiłam odświeżanie rankingu.

--- kalambury-maven/src/main/java/pl/uj/edu/tcs/kalambury_maven/view/Ranking.java
+++ kalambury-maven/src/main/java/pl/uj/edu/tcs/kalambury_maven/view/Ranking.java
@@ -11,6 +11,8 @@ import java.util.EventObject;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
@@ -28,6 +30,10 @@ import javax.swing.table.TableCellEditor;
 
 public class Ranking extends JPanel {
 	private static final long serialVersionUID = 2317946112772863729L;
+	private JTable table;
+	private JLabel whoIsDrawing, whoIsDrawingInfo;
+	private GridBagConstraints tableConstraints, whoIsDrawingConstraints,
+			whoIsDrawingInfoConstraints;
 
 	/**
 	 * Pomocnicza funkcja przerabiająca mapę na posotrowaną listę
@@ -55,39 +61,12 @@ public class Ranking extends JPanel {
 	}
 
 	/**
-	 * Wyświetla ładnie posortowany ranking
-	 * 
-	 * @param users
-	 *            - mapa z nazw graczy w liczbę ich punktów
-	 * @param nowDrawingName
-	 *            - użytkownik, który teraz rysuje
+	 * Tworzenie tabelki
+	 * @param list
+	 * 				- lista, z której tworzę tabelkę posortowaną po liczbie punktów
+	 * @return tabelka posortowana po liczbie punktów
 	 */
-	private void displayRanking(Map<String, Integer> users,
-			String nowDrawingName) {
-		ArrayList<Entry<String, Integer>> list = sortedList(users);
-
-		this.setLayout(new GridBagLayout());
-
-		String formattedName = String.format("<html><a><b>%s</b></a></html>",
-				nowDrawingName);
-		JLabel whoIsDrawing = new JLabel("Now drawing: ");
-
-		GridBagConstraints constraints = new GridBagConstraints(0, 0, 1, 1,
-				GridBagConstraints.HORIZONTAL, 1,
-				GridBagConstraints.CENTER, UNDEFINED_CONDITION, getInsets(), 0,
-				0);
-
-		add(whoIsDrawing, constraints);
-
-		constraints = new GridBagConstraints(1, 0, 1, 1, GridBagConstraints.HORIZONTAL, 
-		1, GridBagConstraints.CENTER, UNDEFINED_CONDITION,
-				getInsets(), 0, 0);
-		JLabel whoIsDrawingInfo = new JLabel(formattedName);
-
-		whoIsDrawingInfo.setBackground(new Color(0xCC5555));
-		// whoIsDrawingInfo.setBorder(BorderFactory.createLineBorder(Color.darkGray));
-		add(whoIsDrawingInfo, constraints);
-
+	private JTable createTable(ArrayList<Entry<String, Integer>> list) {
 		String[] columnNames = { "No.", "Username", "Points" };
 		Object[][] data = new Object[list.size() + 1][3];
 
@@ -100,12 +79,12 @@ public class Ranking extends JPanel {
 			data[i + 1][1] = list.get(i).getKey();
 			data[i + 1][2] = list.get(i).getValue();
 		}
-		JTable table = new JTable(data, columnNames);
+		JTable newTable = new JTable(data, columnNames);
 
-		table.getColumnModel().getColumn(0).setPreferredWidth(30);
-		table.getColumnModel().getColumn(1).setPreferredWidth(120);
-		table.getColumnModel().getColumn(2).setPreferredWidth(60);
-		table.setCellEditor(new TableCellEditor() {
+		newTable.getColumnModel().getColumn(0).setPreferredWidth(30);
+		newTable.getColumnModel().getColumn(1).setPreferredWidth(120);
+		newTable.getColumnModel().getColumn(2).setPreferredWidth(60);
+		newTable.setCellEditor(new TableCellEditor() {
 			@Override
 			public boolean stopCellEditing() {
 				return false;
@@ -144,13 +123,61 @@ public class Ranking extends JPanel {
 				return null;
 			}
 		});
-		table.setBackground(new Color(0xCCCCFF));
-		table.setVisible(true);
-		table.setBorder(BorderFactory.createLineBorder(Color.darkGray));
-		constraints = new GridBagConstraints(0, 1, 2, 3, CENTER_ALIGNMENT,
-				4, GridBagConstraints.CENTER,
+		newTable.setBackground(new Color(0xCCCCFF));
+		newTable.setVisible(true);
+		newTable.setBorder(BorderFactory.createLineBorder(Color.darkGray));
+		
+		return newTable;
+	}
+
+	/**
+	 * Wyświetlanie, kto aktualnie rysuje
+	 * @param nowDrawingName
+	 * 					- nazwa aktualnie rysującego użytkownika
+	 * @return
+	 * 			- miejsce z informacją kto aktualnie rysuje
+	 */
+	private JLabel setWhoIsDrawingInfo(String nowDrawingName) {
+		String formattedName = String.format("<html><a><b>%s</b></a></html>",
+				nowDrawingName);
+		return new JLabel(formattedName);
+	}
+	
+	/**
+	 * Wyświetla ładnie posortowany ranking
+	 * 
+	 * @param users
+	 *            - mapa z nazw graczy w liczbę ich punktów
+	 * @param nowDrawingName
+	 *            - użytkownik, który teraz rysuje
+	 */
+	public void displayRanking(Map<String, Integer> users,
+			String nowDrawingName) {
+		ArrayList<Entry<String, Integer>> list = sortedList(users);
+
+		if(this.getComponents().length != 0) {
+			for(Component c : this.getComponents())
+				c.setVisible(false);
+			this.removeAll();
+		}
+		this.setLayout(new GridBagLayout());
+
+		table = createTable(list);
+		whoIsDrawing = new JLabel("Now drawing: ");
+		whoIsDrawingInfo = setWhoIsDrawingInfo("?");
+		tableConstraints = new GridBagConstraints(0, 1, 2, 3, CENTER_ALIGNMENT,
+				4, GridBagConstraints.CENTER, UNDEFINED_CONDITION, getInsets(),
+				0, 0);
+		whoIsDrawingConstraints = new GridBagConstraints(0, 0, 1, 1,
+				GridBagConstraints.HORIZONTAL, 1, GridBagConstraints.CENTER,
 				UNDEFINED_CONDITION, getInsets(), 0, 0);
-		add(table, constraints);
+		whoIsDrawingInfoConstraints = new GridBagConstraints(1, 0, 1, 1,
+				GridBagConstraints.HORIZONTAL, 1, GridBagConstraints.CENTER,
+				UNDEFINED_CONDITION, getInsets(), 0, 0);
+		this.add(table, tableConstraints);
+		this.add(whoIsDrawing, whoIsDrawingConstraints);
+		this.add(whoIsDrawingInfo, whoIsDrawingInfoConstraints);
+		this.revalidate();
 	}
 
 	/**
@@ -177,5 +204,19 @@ public class Ranking extends JPanel {
 		Ranking r = new Ranking(users);
 		frame.getContentPane().add(r);
 		frame.setVisible(true);
+
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		users.put("prum", 15);
+		users.put("Lama", 1000);
+		r.displayRanking(users, "?");
+		users.put("lelele", 15);
+		users.put("nuda", 1000);
+		r.displayRanking(users, "?");
 	}
 }
