b8a4d8723ed7cac0aa0a81999cc1753c0223d7fa
mreve <aadymek@gmail.com>
2013-05-31 12:35:58 +0200

    Więcej testów (i drobne dodatki do GameLogic, dla usprawnienia testów + korekta jednej bezsensownej, potencjalnie bugogennej instrukcji w game logic).

--- kalambury-maven/src/test/java/pl/uj/edu/tcs/kalambury_maven/GameLogicTest.java
+++ kalambury-maven/src/test/java/pl/uj/edu/tcs/kalambury_maven/GameLogicTest.java
@@ -6,13 +6,17 @@ import java.util.List;
 import pl.uj.edu.tcs.kalambury_maven.event.ClearScreenEvent;
 import pl.uj.edu.tcs.kalambury_maven.event.Event;
 import pl.uj.edu.tcs.kalambury_maven.event.MessageSendEvent;
+import pl.uj.edu.tcs.kalambury_maven.event.NewMessageWrittenEvent;
 import pl.uj.edu.tcs.kalambury_maven.event.NewPointsDrawnEvent;
+import pl.uj.edu.tcs.kalambury_maven.event.NewWordForGuessingEvent;
 import pl.uj.edu.tcs.kalambury_maven.event.NewWordIsNeededEvent;
 import pl.uj.edu.tcs.kalambury_maven.event.NextRoundStartsEvent;
 import pl.uj.edu.tcs.kalambury_maven.event.ResetUserRankingEvent;
 import pl.uj.edu.tcs.kalambury_maven.event.StartDrawingEvent;
 import pl.uj.edu.tcs.kalambury_maven.event.UsersOfflineEvent;
 import pl.uj.edu.tcs.kalambury_maven.event.UsersOnlineEvent;
+import pl.uj.edu.tcs.kalambury_maven.event.WordGuessedEvent;
+import pl.uj.edu.tcs.kalambury_maven.model.Point;
 import pl.uj.edu.tcs.kalambury_maven.server.GameLogic;
 import pl.uj.edu.tcs.kalambury_maven.server.Server;
 import junit.framework.Test;
@@ -52,6 +56,8 @@ public class GameLogicTest extends TestCase {
 	public void UsersOnlineEventTest(String testname) {
 //		System.out.println("----> UsersOnlineEvent-test");
 		
+		assertTrue(packOfLogs.size() == packOfNames.size() && packOfLogs.size() >= 4);
+		
 		assertEquals(ResetUserRankingEvent.class, packOfLogs.get(0).getClass());
 		assertEquals(
 				testGameLogic.getModel().getUserRanking().getFullRanking(),
@@ -84,6 +90,7 @@ public class GameLogicTest extends TestCase {
 	}
 	public void UsersOfflineEventTest(String testname) {
 //		System.out.println("----> UsersOfflineEvent-test");
+		assertTrue(packOfLogs.size() == packOfNames.size());
 		
 		if (packOfLogs.size() > 1) {
 			assertEquals("everyone", packOfNames.get(0));
@@ -99,6 +106,8 @@ public class GameLogicTest extends TestCase {
 					packOfLogs.get(packOfNames.size() - 1).getClass());
 
 		} else {
+			assertTrue(packOfLogs.size() == 1);
+			
 			assertEquals("everyone", packOfNames.get(0));
 			assertEquals(UsersOfflineEvent.class, packOfLogs.get(0).getClass());
 		}
@@ -107,6 +116,7 @@ public class GameLogicTest extends TestCase {
 	}
 	public void startNextRoundTest() {
 //		System.out.println("----> startNextRound-test");
+		assertTrue(packOfLogs.size() >= 3);
 		
 		assertEquals(ClearScreenEvent.class, packOfLogs.get(0).getClass());
 		assertEquals("everyone", packOfNames.get(0));
@@ -118,14 +128,79 @@ public class GameLogicTest extends TestCase {
 		
 		assertEquals(NewWordIsNeededEvent.class, packOfLogs.get(2).getClass());
 		assertEquals(testGameLogic.getQueue().peek(), packOfNames.get(2));
+		
 	}
 	public void NewWordForGuessingEventTest(String testname) {
 		if(testGameLogic.getQueue().peek() == testname) {
+			assertTrue(packOfLogs.size() == packOfNames.size() && packOfLogs.size() == 1);
+			
 			assertEquals(StartDrawingEvent.class, packOfLogs.get(0).getClass());
 			assertEquals(testname, packOfNames.get(0));
 		}
+		packOfLogs.clear();
+		packOfNames.clear();
 	}
-	
+	public void ClearScreenEventTest(String testname) {
+		assertTrue(packOfLogs.size() == packOfNames.size());
+		if(packOfLogs.size() >= 1) {
+			assertTrue(packOfLogs.size() == 1);
+			
+			assertEquals(ClearScreenEvent.class, packOfLogs.get(0).getClass());
+			assertEquals("everyone", packOfNames.get(0));
+			
+			packOfLogs.clear();
+			packOfNames.clear();
+		}
+	}
+	public void NewPointsDrawnEventTest(String testname) {
+		assertTrue(packOfLogs.size() == packOfNames.size());
+		
+		if(!(testGameLogic.getQueue().isEmpty() || !testGameLogic.isSomeoneDrawing())) {
+			if(testname.equals(testGameLogic.getQueue().peek())) {
+				assertTrue(packOfLogs.size() == 1);
+				
+				assertEquals(NewPointsDrawnEvent.class, packOfLogs.get(0).getClass());
+				assertEquals("everyone", packOfNames.get(0));
+			}	
+		}
+		packOfLogs.clear();
+		packOfNames.clear();
+	}
+	public void NewMessageWrittenEventTest(String testname) {
+		assertTrue(packOfLogs.size() == packOfNames.size());
+		
+		if(!testGameLogic.getQueue().isEmpty() && testname.equals(testGameLogic.getQueue().peek())) {
+			assertTrue(packOfLogs.size() == 1);
+			
+			assertEquals(NewMessageWrittenEvent.class, packOfLogs.get(0).getClass());
+			assertEquals(testname, packOfNames.get(0));
+		}
+		else {
+			assertEquals(MessageSendEvent.class, packOfLogs.get(0).getClass());
+			assertEquals("everyone", packOfNames.get(0));
+			assertEquals(testname, ((MessageSendEvent)packOfLogs.get(0)).getUser());
+			
+			String givenWord = ((MessageSendEvent)packOfLogs.get(0)).getMessage().trim().toLowerCase();
+			String trueWord = testGameLogic.getNowBeingDrawnWord().trim().toLowerCase();
+			if(testGameLogic.isSomeoneDrawing() && givenWord.equals(trueWord)) {
+				
+				assertEquals(WordGuessedEvent.class, packOfLogs.get(1).getClass());
+				assertEquals(testname, packOfNames.get(1));
+				assertEquals(testGameLogic.getNowBeingDrawnWord(), ((WordGuessedEvent) packOfLogs.get(1)).getWord());
+				
+				packOfLogs.remove(0); packOfLogs.remove(0);
+				packOfNames.remove(0); packOfNames.remove(0);
+				
+				startNextRoundTest();
+			}
+			else if(packOfLogs.size() == 4) { //był event z areSimilar()
+				assertEquals(MessageSendEvent.class, packOfLogs.get(3));
+				assertEquals(testname, packOfNames.get(3));
+			}
+		}
+		packOfLogs.clear();
+		packOfNames.clear();
+	}
 	
 	/**
 	 * simple one in - one out test.
@@ -172,5 +247,48 @@ public class GameLogicTest extends TestCase {
 
 	}
 
+	/**
+	 * some in - some drawing - some in -
+	 * 		- some drawing - some out - some drawing 
+	 * 		- everybody out test.
+	 */
+	public void test02_simple_drawing() {
+		testGameLogic.setServer(testServer);
+		
+		//logowanie dwóch użytkowników
+		testGameLogic.reactTo("test1", new UsersOnlineEvent("test1"));
+		UsersOnlineEventTest("test1");
+		testGameLogic.reactTo("test2", new UsersOnlineEvent("test2"));
+		UsersOnlineEventTest("test2");
+		//pierwszy podaje słowo do zgadnięcia, po czym oboje próbują rysować
+		testGameLogic.reactTo("test1", new NewWordForGuessingEvent("kot"));
+		NewWordForGuessingEventTest("test1");
+		testGameLogic.reactTo("test1", new NewPointsDrawnEvent(new ArrayList<Point>()));
+		NewPointsDrawnEventTest("test1");
+		testGameLogic.reactTo("test2", new NewPointsDrawnEvent(new ArrayList<Point>()));
+		NewPointsDrawnEventTest("test2");
+		//pojawia się nowy użytkownik i próbuje podać hasło do rysowania
+		testGameLogic.reactTo("test3", new UsersOnlineEvent("test3"));
+		UsersOnlineEventTest("test3");
+		testGameLogic.reactTo("test3", new NewWordForGuessingEvent("kot"));
+		NewWordForGuessingEventTest("test3");
+		//znika użytkownik drugi; użytkownik pierwszy nadal rysuje
+		testGameLogic.reactTo("test2", new UsersOfflineEvent("test2"));
+		UsersOfflineEventTest("test2");
+		testGameLogic.reactTo("test1", new NewPointsDrawnEvent(new ArrayList<Point>()));
+		NewPointsDrawnEventTest("test1");
+		//znika użytkownik pierwszy (teraz rysuje trójka, ale najpierw musi podać hasło)
+		testGameLogic.reactTo("test1", new UsersOfflineEvent("test1"));
+		UsersOfflineEventTest("test1");
+		testGameLogic.reactTo("test3", new NewPointsDrawnEvent(new ArrayList<Point>()));
+		NewPointsDrawnEventTest("test3");
+		testGameLogic.reactTo("test3", new NewWordForGuessingEvent("kot"));
+		NewWordForGuessingEventTest("test3");
+		testGameLogic.reactTo("test3", new NewPointsDrawnEvent(new ArrayList<Point>()));
+		NewPointsDrawnEventTest("test3");
+		//trójka wychodzi
+		testGameLogic.reactTo("test3", new UsersOfflineEvent("test3"));
+		UsersOfflineEventTest("test3");
+	}
 
 }
